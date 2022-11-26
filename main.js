9901f5a66794ca0526abc07eb9212fa1fb8cf834
Gabriel-Suldine <gabriel.suldinne@gmail.com>
2022-11-26 12:39:39 -0300

    API-funcional

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import './style.css'
 import '@picocss/pico';
 const formConsultarConta = document.querySelector('#consultarConta');
-const inputConta = formConsultarConta.cep;
+const inputConta = formConsultarConta.name;
 const divDados = document.querySelector('#dados')
 const btnConsultarConta = document.querySelector('#btnConsultarConta')
-
 
 formConsultarConta.addEventListener('submit', event => {
   event.preventDefault()  //anula o comportamento padrão de envio do form  
@@ -18,10 +17,13 @@ async function consultarConta(conta) {
   let response = await fetch(`https://api.github.com/users/${conta}`)
   let dadosConta = await response.json()
   divDados.innerHTML = `
-    <p>Nome: ${dadosConta.name} </p>    
+    <p>Nome: ${dadosConta.name} </p>     
+    <img src="${dadosConta.avatar_url}">
+    <a href="${dadosConta.html_url}"> Perfil no GitHub</a>   
 `
 ativaLoader(false)
 }
+
 
 function ativaLoader(ativo) {
   if (ativo) {
@@ -32,6 +34,3 @@ function ativaLoader(ativo) {
     btnConsultarConta.textContent = 'Consultar'
   }
 }
-
-
-
